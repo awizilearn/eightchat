80d6718d99753b1bd93037106bff39b236451a89
Admin WIzi Learn <awizilearn@gmail.com>
2025-12-10 11:46:00 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,9 @@
-// This file is intentionally left blank. 
-// The Firebase configuration will be populated automatically by Firebase App Hosting.
-export const firebaseConfig = {};
+// This file is populated with the Firebase configuration for your web app.
+export const firebaseConfig = {
+  "projectId": "studio-8669415397-a6d87",
+  "appId": "1:866782566182:web:3a83b318e40786e15a53e7",
+  "apiKey": "AIzaSyDgG5PtRCJ7ezktfm3outnzYQI38qq9wCE",
+  "authDomain": "studio-8669415397-a6d87.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "866782566182"
+};
